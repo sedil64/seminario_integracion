33ba779595ca2bd0a3e6e16c45b4d719c61eae4a
sedil64 <dilan6465@gmail.com>
2025-12-11 20:37:20 -0500

    FIX: settings

--- django/football_api/football_api/settings.py
+++ django/football_api/football_api/settings.py
@@ -102,7 +102,10 @@ TIME_ZONE = 'America/Guayaquil'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
